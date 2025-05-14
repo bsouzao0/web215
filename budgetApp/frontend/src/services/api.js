63bdcc4402b6ee374c71bdda01f52af94164e4cd
Brenda Sayonara <149035540+bsouzao0@users.noreply.github.com>
2025-05-13 21:07:35 -0400

    d

--- budgetApp/frontend/src/services/api.js
+++ budgetApp/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://vite-react-six-gamma-66.vercel.app/',
 });
 
 API.interceptors.request.use((req) => {
